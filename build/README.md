3609bc1813c692d0540bfd9d7efbd446749cc166
Michele Locati <michele@locati.it>
2023-03-22 14:37:09 +0100

    Build also CSS files

---
+++ build/README.md
@@ -0,0 +1,19 @@
+## Setup
+
+`npm ci`
+
+## Build
+
+- for development
+  `npm run dev`
+- for production
+  `npm run prod`
+
+## Sources
+
+- imagesloaded
+  https://github.com/desandro/imagesloaded
+- jquery.knob
+  https://github.com/aterrien/jQuery-Knob
+- jquery.prettyPhoto
+  https://github.com/scaron/prettyphoto
--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
+const BLOCK_DIR = '../blocks/easy_image_slider';
+
 mix
     .disableNotifications()
     .webpackConfig({
@@ -19,13 +21,23 @@ mix
         },
     })
 ;
-mix.setPublicPath('../blocks/easy_image_slider');
+mix.setPublicPath(BLOCK_DIR);
 
 mix
     .js('src/block-edit.js', 'js/block-edit.js')
-    .js('src/imagesloaded.pkgd.js', 'js/imagesloaded.js')
+    .css('src/block-edit.css', 'css/block-edit.css')
+    .css('src/block-view.css', 'css/block-view.css')
+    .js('src/imagesloaded/imagesloaded.pkgd.js', 'js/imagesloaded.js')
     .js('src/intense.js', 'js/intense.js')
     .js('src/jquery.knob.js', 'js/jquery.knob.js')
-    .js('src/jquery.prettyPhoto.js', 'js/jquery.prettyPhoto.js')
-    .js('src/owl.carousel.js', 'js/owl.carousel.js')
+    .js('src/jquery.prettyPhoto/jquery.prettyPhoto.js', 'js/jquery.prettyPhoto.js')
+    .css('src/jquery.prettyPhoto/prettyPhoto.css', 'css/prettyPhoto.css', {processUrls: false})
+    .copy('src/images/prettyPhoto/', BLOCK_DIR + '/images/prettyPhoto')
+    .js('src/owl.carousel/owl.carousel.js', 'js/owl.carousel.js')
+    .css('src/owl.carousel/owl.carousel.css', 'css/owl.carousel.css', {processUrls: false})
+    .copy('src/owl.carousel/grabbing.png', BLOCK_DIR + '/css/grabbing.png')
+    .css('src/owl.carousel/owl.theme.css', 'css/owl.theme.css', {processUrls: false})
+    .copy('src/owl.carousel/AjaxLoader.gif', BLOCK_DIR + '/css/AjaxLoader.gif')
+    .css('src/owl.carousel/owl.transitions.css', 'css/owl.transitions.css')
+    .css('src/animate/animate.css', 'css/animate.css')
 ;
