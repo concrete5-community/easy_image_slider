e7562811e2f92957935baeed1647e34879ded040
Michele Locati <michele@locati.it>
2025-06-05 15:36:58 +0200

    Use SVG placeholders (#16)

--- blocks/easy_image_slider/templates/one_slide/view.php
+++ blocks/easy_image_slider/templates/one_slide/view.php
@@ -8,9 +8,10 @@ defined('C5_EXECUTE') or die('Access Denied.');
 
 /**
  * @var Concrete\Core\Block\View\BlockView|Concrete\Core\Page\Type\Composer\Control\BlockControl $this
+ * @var Concrete\Package\EasyImageSlider\Block\EasyImageSlider\Controller $controller
  * @var EasyImageSlider\Options $options
  * @var int|string $bID
- * @var Concrete\Core\File\File[] $files
+ * @var Concrete\Core\Entity\File\File[] $files
  */
 
 $c = Page::getCurrentPage();
@@ -35,7 +36,7 @@ if ($c->isEditMode()) {
                 // Different Thumbnails sizes
                 $thumbnailUrl = $options->isSingleItemSlide() ? $f->getRelativePath() : $f->getThumbnailURL($type->getBaseVersion());
                 // Due to a bug in OWL2 Lazy work only with loop activated.
-                $placeHolderUrl = $options->lazy ? ($this->getBlockURL() . "/images/placeholders/placeholder-{$f->getAttribute('width')}-{$f->getAttribute('height')}.png") : $thumbnailUrl;
+                $placeHolderUrl =  $options->lazy ? $controller->getPlaceholderUrl($f) : $thumbnailUrl;
                 $retinaThumbnailUrl = $options->isSingleItemSlide() ? $f->getRelativePath() : $f->getThumbnailURL($type->getDoubledVersion());
                 // Styles for color on hover
                 $thumbnailBackground = $options->isTransparent ? 'background-color:transparent' : ('background-color:' . ($f->getAttribute('image_bg_color') ? $f->getAttribute('image_bg_color') : $options->fadingColor) . ';');
